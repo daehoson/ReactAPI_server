952fcb43de0b20ae59c706249fc762a078b12320
daehoson <daehoson@naver.com>
2024-08-30 11:56:42 +0900

    - 리액트강의용 스프링부트 프로젝트 crud 나머지 코드 커밋 - 나중에 또 작성 예정

--- src/main/java/org/daeho/apiserver/controller/TodoController.java
+++ src/main/java/org/daeho/apiserver/controller/TodoController.java
@@ -2,12 +2,13 @@ package org.daeho.apiserver.controller;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
+import org.daeho.apiserver.dto.PageRequestDTO;
+import org.daeho.apiserver.dto.PageResponseDTO;
 import org.daeho.apiserver.dto.TodoDTO;
 import org.daeho.apiserver.service.TodoService;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Map;
 
 @RestController
 @Log4j2
@@ -20,4 +21,30 @@ public class TodoController {
         return todoService.get(tno);
     }
 
+    @GetMapping("/list")
+    public PageResponseDTO<TodoDTO> list(PageRequestDTO pageRequestDTO){
+        log.info("list......................" + pageRequestDTO);
+        return todoService.getList(pageRequestDTO);
+    }
+
+    @PostMapping("/")
+    public Map<String,Long> register(@RequestBody TodoDTO dto){
+        log.info("todoDTO: "+dto);
+        Long tno = todoService.register(dto);
+        return Map.of("TNO", tno);
+    }
+
+    @PutMapping("/{tno}")
+    public Map<String, String> modify(@PathVariable("tno") Long tno,
+                                      @RequestBody TodoDTO todoDTO){
+        todoDTO.setTno(tno);
+        todoService.modify(todoDTO);
+        return Map.of("RESULT","SUCCESS");
+    }
+
+    @DeleteMapping("/{tno}")
+    public Map<String, String> remove(@PathVariable Long tno){
+        todoService.remove(tno);
+        return Map.of("RESULT","SUCCESS");
+    }
 }
